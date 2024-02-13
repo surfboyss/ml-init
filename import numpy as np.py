91995659e7f08f4f966206dd0422b541bb607043
surfboyss <errajiilyass01@gmail.com>
2024-02-13 22:05:13 +0100

    push

--- import numpy as np.py
+++ import numpy as np.py
@@ -2,5 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('/Users/ilyasserraji/desktop/titanic3.numbers')
-tt
+data = pd.read_csv('/Users/ilyasserraji/desktop/titanic3.csv',delimiter=';')
+print(data.shape)
+print(data.columns)
